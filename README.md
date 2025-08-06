24837ec05029ba3905dc47d6ef48dd25d05281c0
Veronika Belousova <belousovanica@gmail.com>
2025-08-06 14:28:10 +0300

    Задание 2 выполнено

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sort Heroes
+
+![Node.js CI](https://github.com/NixonGH/ajs-unit-testing2/actions/workflows/node.js.yml/badge.svg)
---
+++ src/js/__tests__/sortHeroes.test.js
@@ -0,0 +1,28 @@
+import sortHeroesByHealth from '../sortHeroes';
+
+test('sort heroes by health in descending order', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = sortHeroesByHealth(input);
+
+  expect(result).toEqual(expected);
+});
+
+test('check if the array is original', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const copy = [...input];
+  sortHeroesByHealth(input);
+  expect(input).toEqual(copy);
+});
